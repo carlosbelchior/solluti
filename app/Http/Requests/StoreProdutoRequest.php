472c524064a6f9992885097571a5b41d8e42ef1c
Carlos Belchior <carlosbelchiorsilva@gmail.com>
2022-07-24 15:38:50 -0300

    Form Requests

---
+++ app/Http/Requests/StoreProdutoRequest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class StoreProdutoRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'nome' => 'required|min:3|max:40',
+            'valor' => 'required|integer|min:2|max:6',
+            'loja_id' => 'required|exists:lojas,id',
+            'ativo' => 'required|boolean'
+        ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'message' => $validator->errors(),
+            'status' => 'error',
+            'type' => 'validation'
+        ], 400));
+    }
+}
---
+++ app/Http/Requests/UpdateLojaRequest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Validation\Rule;
+
+class UpdateLojaRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        $id = $this->route('id');
+        return [
+            'nome' => 'required|min:3|max:40',
+            'email' => [
+                'required', 'email',
+                Rule::unique('lojas', 'email')->where(fn ($query) => $query->where('id', '!=', $id))
+            ]
+        ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'message' => $validator->errors(),
+            'status' => 'error',
+            'type' => 'validation'
+        ], 400));
+    }
+}
